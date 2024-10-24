dd5f400eaabdeaa653f916e195afdaca506a1825
melindalina <melindalina300@gmail.com>
2024-10-24 19:01:25 +0800

    Add files via upload

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -1,21 +1,20 @@
 const command = {
     command: ['menu2'],
-    category: ['main']
+    category: ['info']
 };
 
 command.script = async (m, { hisoka }) => {
-    const commands = hisoka.getCommand;
+    const commandsList = Object.values(hisoka.getCommand('all:commands'));
+    let menuText = '🌟 *Menu Commands* 🌟\n\n';
 
-    let text = `Halo Dek @${m.sender.split`@`[0]}, Ini Menu:\n\n`;
+    commandsList.forEach(plugin => {
+        const commandNames = plugin.command.join(', ');
+        const category = plugin.category.join(', ');
+        menuText += `🔹 *Commands:* ${commandNames}\n`;
+        menuText += `🔹 *Category:* ${category}\n\n`;
+    });
 
-    for (const [fileName, plugin] of commands['all:commands']) {
-        if (plugin.command && plugin.category) {
-            text += `*${plugin.category[0]}*\n`;
-            text += `└─ ${plugin.command.join(', ')}\n\n`;
-        }
-    }
-
-    await m.reply(text, { mentions: [m.sender] });
+    await m.reply(menuText);
 };
 
 export default command;
